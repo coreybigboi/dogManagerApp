4287e5a8c3f472c446dceea10a0fca65da245d87
BARR <ITS_BARRCO@health.gov.au>
2024-03-13 14:01:36 +1100

    adds tests

---
+++ src/test/java/com/dogmanagerapp/controllers/DogControllerTest.java
@@ -0,0 +1,27 @@
+package com.dogmanagerapp.controllers;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.runtime.server.EmbeddedServer;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@MicronautTest
+public class DogControllerTest {
+
+    @Inject
+    EmbeddedServer server;
+
+    @Inject
+    @Client("/")
+    HttpClient client;
+
+    @Test
+    void testDogResponse() {
+        String response = client.toBlocking().retrieve(HttpRequest.GET("/api/dogs"));
+        assertEquals("dogs works.", response);
+    }
+}
