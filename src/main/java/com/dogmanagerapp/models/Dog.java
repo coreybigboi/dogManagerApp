4722aa7327960a9ea5a6b4f387a44478457b27fe
BARR <ITS_BARRCO@health.gov.au>
2024-03-13 14:08:42 +1100

    adds dog model

---
+++ src/main/java/com/dogmanagerapp/models/Dog.java
@@ -0,0 +1,25 @@
+package com.dogmanagerapp.models;
+
+public class Dog {
+    private final String name;
+    private final String breed;
+    private final int age;
+
+    public Dog(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
